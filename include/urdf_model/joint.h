53bc81e6e58599be9145067fd8781d73f9739b86
Aarav Gupta <aarav@spikonado.com>
2026-02-10 00:45:42 +0530

    Update include/urdf_model/joint.h
    Co-authored-by: Sai Kishor Kothakota <saisastra3@gmail.com>
    Signed-off-by: Aarav Gupta <aarav@spikonado.com>

--- include/urdf_model/joint.h
+++ include/urdf_model/joint.h
@@ -77,8 +77,8 @@ public:
 
   void clear()
   {
-    lower = -std::numeric_limits<double>::infinity();
-    upper = std::numeric_limits<double>::infinity();
+    lower = -std::numeric_limits<double>::quiet_NaN();
+    upper = std::numeric_limits<double>::quiet_NaN();
     effort = std::numeric_limits<double>::infinity();
     velocity = std::numeric_limits<double>::infinity();
     acceleration = std::numeric_limits<double>::infinity();
